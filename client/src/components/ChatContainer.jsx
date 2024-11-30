61a8a1d91f353709205e345e93b686d9334c9bdc
ryuga004 <rahul0singh002@gmail.com>
2024-11-30 10:00:55 +0530

    Update ChatContainer.jsx

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -6,8 +6,8 @@ import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import { sendMessageRoute, recieveMessageRoute } from "../utils/APIRoutes";
 const REACT_APP_LOCALHOST_KEY = "LoggedIn";
-export default function ChatContainer({ currentChat, socket }) {
   axios.defaults.withCredentials = true;
+export default function ChatContainer({ currentChat, socket }) {
   const [messages, setMessages] = useState([]);
   const scrollRef = useRef();
   const [arrivalMessage, setArrivalMessage] = useState(null);

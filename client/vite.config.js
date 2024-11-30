b6066c55e570bda92b8b850b93964abe6006872a
ryuga004 <rahul0singh002@gmail.com>
2024-11-30 10:08:42 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://live-chatapi.vercel.app', // Backend URL
+        changeOrigin: true,
+        secure: false, // Set to true if the backend uses HTTPS with a valid certificate
+        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Remove '/api' from requests
+      },
+    },
+  },
+});
